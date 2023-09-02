cefcec8d354c746c72feb720245de1d6b227f320
renataBelila <renata.belila1@gmail.com>
2023-09-01 22:01:00 -0300

    Configurando as rotas

---
+++ my-create-app/src/routes/EditarProdutos.jsx
@@ -0,0 +1,11 @@
+export default function EditarProduto(){
+    return(
+        <>
+        <main>
+            <h1>
+                Editando o Produto
+            </h1>
+        </main>
+        </>
+    )
+}
